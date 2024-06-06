6fc444c60c96b975c8cc41bf0fcb8b31475caa2e
wandaynr <129712187+wandaynr@users.noreply.github.com>
2024-06-06 11:47:33 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -15,42 +15,41 @@ public class Main {
                 case 1:
                     System.out.print("Masukkan panjang sisi persegi: ");
                     int sisiPersegi = scanner.nextInt();
-                    Persegi persegi = new Persegi(sisiPersegi);
-                    System.out.println("Luas Persegi: " + persegi.luas());
-                    System.out.println("Keliling Persegi: " + persegi.keliling());
+                    BangunDatar persegi = new BangunDatar(sisiPersegi);
+                    System.out.println("Luas Persegi: " + persegi.luasPersegi());
+                    System.out.println("Keliling Persegi: " + persegi.kelilingPersegi());
                     break;
                 case 2:
                     System.out.print("Masukkan panjang persegi panjang: ");
                     int panjang = scanner.nextInt();
                     System.out.print("Masukkan lebar persegi panjang: ");
                     int lebar = scanner.nextInt();
-                    PersegiPanjang persegiPanjang = new PersegiPanjang(panjang, lebar);
-                    System.out.println("Luas Persegi Panjang: " + persegiPanjang.luas());
-                    System.out.println("Keliling Persegi Panjang: " + persegiPanjang.keliling());
+                    BangunDatar persegiPanjang = new BangunDatar(panjang, lebar);
+                    System.out.println("Luas Persegi Panjang: " + persegiPanjang.luasPersegiPanjang());
+                    System.out.println("Keliling Persegi Panjang: " + persegiPanjang.kelilingPersegiPanjang());
                     break;
                 case 3:
                     System.out.print("Masukkan panjang alas segitiga: ");
                     double alas = scanner.nextDouble();
                     System.out.print("Masukkan tinggi segitiga: ");
                     double tinggi = scanner.nextDouble();
-                    Segitiga segitiga = new Segitiga(alas, tinggi);
-                    System.out.println("Luas Segitiga: " + segitiga.luas());
-                    // keliling segitiga tidak dihitung karena kita tidak punya panjang sisi lainnya
+                    BangunDatar segitiga = new BangunDatar(alas, tinggi, true);
+                    System.out.println("Luas Segitiga: " + segitiga.luasSegitiga());
                     break;
                 case 4:
                     System.out.print("Masukkan diameter lingkaran: ");
                     double diameter = scanner.nextDouble();
-                    Lingkaran lingkaran = new Lingkaran(diameter);
-                    System.out.println("Luas Lingkaran: " + lingkaran.luas());
-                    System.out.println("Keliling Lingkaran: " + lingkaran.keliling());
+                    BangunDatar lingkaran = new BangunDatar(diameter);
+                    System.out.println("Luas Lingkaran: " + lingkaran.luasLingkaran());
+                    System.out.println("Keliling Lingkaran: " + lingkaran.kelilingLingkaran());
                     break;
                 case 5:
                     System.out.print("Masukkan panjang sisi kubus: ");
                     int sisiKubus = scanner.nextInt();
-                    Kubus kubus = new Kubus(sisiKubus);
-                    System.out.println("Volume Kubus: " + kubus.volume());
-                    System.out.println("Luas Permukaan Kubus: " + kubus.luasPermukaan());
-                    System.out.println("Keliling Kubus: " + kubus.keliling());
+                    BangunRuang kubus = new BangunRuang(sisiKubus);
+                    System.out.println("Volume Kubus: " + kubus.volumeKubus());
+                    System.out.println("Luas Permukaan Kubus: " + kubus.luasPermukaanKubus());
+                    System.out.println("Keliling Kubus: " + kubus.kelilingKubus());
                     break;
                 case 6:
                     System.out.print("Masukkan panjang balok: ");
@@ -59,18 +58,18 @@ public class Main {
                     int lebarBalok = scanner.nextInt();
                     System.out.print("Masukkan tinggi balok: ");
                     int tinggiBalok = scanner.nextInt();
-                    Balok balok = new Balok(panjangBalok, lebarBalok, tinggiBalok);
-                    System.out.println("Volume Balok: " + balok.volume());
-                    System.out.println("Luas Permukaan Balok: " + balok.luasPermukaan());
-                    System.out.println("Keliling Balok: " + balok.keliling());
+                    BangunRuang balok = new BangunRuang(panjangBalok, lebarBalok, tinggiBalok);
+                    System.out.println("Volume Balok: " + balok.volumeBalok());
+                    System.out.println("Luas Permukaan Balok: " + balok.luasPermukaanBalok());
+                    System.out.println("Keliling Balok: " + balok.kelilingBalok());
                     break;
                 case 7:
                     System.out.print("Masukkan diameter bola: ");
-                    int diameterBola = scanner.nextInt();
-                    Bola bola = new Bola(diameterBola);
-                    System.out.println("Volume Bola: " + bola.volume());
-                    System.out.println("Luas Permukaan Bola: " + bola.luasPermukaan());
-                    System.out.println("Keliling Bola: " + bola.keliling());
+                    double diameterBola = scanner.nextDouble();
+                    BangunRuang bola = new BangunRuang(diameterBola);
+                    System.out.println("Volume Bola: " + bola.volumeBola());
+                    System.out.println("Luas Permukaan Bola: " + bola.luasPermukaanBola());
+                    System.out.println("Keliling Bola: " + bola.kelilingBola());
                     break;
                 case 8:
                     System.out.println("Terima kasih! Program selesai.");
