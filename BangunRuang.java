644f22f3c2b80bc92a94bac426f4567853d91355
wandaynr <129712187+wandaynr@users.noreply.github.com>
2024-06-06 11:46:46 +0700

    Update BangunRuang.java

--- BangunRuang.java
+++ BangunRuang.java
@@ -1,143 +1,74 @@
-package week9;
-
-// Kelas abstrak BangunRuang
-abstract class BangunRuang {
-    public abstract double volume();
-    public abstract double luasPermukaan();
-    public abstract double keliling();
-}
-
-// Kelas untuk Kubus
-class Kubus extends BangunRuang {
-    private int sisi;
-
-    public Kubus(int sisi) {
-        this.sisi = sisi;
-    }
-
-    public double volume() {
-        return Math.pow(sisi, 3);
-    }
-
-    public double luasPermukaan() {
-        return 6 * Math.pow(sisi, 2);
-    }
-
-    public double keliling() {
-        return 12 * sisi;
-    }
-}
-
-// Kelas untuk Balok
-class Balok extends BangunRuang {
-    private int panjang;
-    private int lebar;
-    private int tinggi;
-
-    public Balok(int panjang, int lebar, int tinggi) {
-        this.panjang = panjang;
-        this.lebar = lebar;
-        this.tinggi = tinggi;
-    }
-
-    public double volume() {
-        return panjang * lebar * tinggi;
-    }
-
-    public double luasPermukaan() {
-        return 2 * (panjang * lebar + panjang * tinggi + lebar * tinggi);
-    }
-
-    public double keliling() {
-        return 4 * (panjang + lebar + tinggi);
-    }
-}
-
-// Kelas untuk Bola
-class Bola extends BangunRuang {
-    private int diameter;
-    private static final double PI = 3.14;
-
-    public Bola(int diameter) {
-        this.diameter = diameter;
-    }
-
-    public double volume() {
-        double radius = diameter / 2.0;
-        return (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
-    }
-
-    public double luasPermukaan() {
-        double radius = diameter / 2.0;
-        return 4 * PI * Math.pow(radius, 2);
-    }
-
-    public double keliling() {
-        double radius = diameter / 2.0;
-        return 2 * PI * radius;
-    }
-}
-
-
-
-
-/*package week9;
+package wanda;
 
 public class BangunRuang {
     private int sisi;
     private int panjang;
     private int lebar;
     private int tinggi;
-    private int diameter;
+    private double diameter;
     private static final double PI = 3.14;
 
-    // konstruktor kubus
+    // Konstruktor untuk Kubus
     public BangunRuang(int sisi) {
         this.sisi = sisi;
     }
 
-    // konstruktor balok
+    // Konstruktor untuk Balok
     public BangunRuang(int panjang, int lebar, int tinggi) {
         this.panjang = panjang;
         this.lebar = lebar;
         this.tinggi = tinggi;
     }
 
-    // konstruktor bola
-    public BangunRuang(double diameterBola) {
-    this.diameter = (int)diameterBola;
-}
+    // Konstruktor untuk Bola
+    public BangunRuang(double diameter) {
+        this.diameter = diameter;
+    }
 
-
-    // menghitung volume kubus
+    // Menghitung volume Kubus
     public double volumeKubus() {
         return Math.pow(sisi, 3);
     }
 
-    // menghitung volume balok
+    // Menghitung luas permukaan Kubus
+    public double luasPermukaanKubus() {
+        return 6 * Math.pow(sisi, 2);
+    }
+
+    // Menghitung keliling Kubus
+    public double kelilingKubus() {
+        return 12 * sisi;
+    }
+
+    // Menghitung volume Balok
     public double volumeBalok() {
         return panjang * lebar * tinggi;
     }
 
-    // menghitung volume bola
+    // Menghitung luas permukaan Balok
+    public double luasPermukaanBalok() {
+        return 2 * (panjang * lebar + panjang * tinggi + lebar * tinggi);
+    }
+
+    // Menghitung keliling Balok
+    public double kelilingBalok() {
+        return 4 * (panjang + lebar + tinggi);
+    }
+
+    // Menghitung volume Bola
     public double volumeBola() {
         double radius = diameter / 2.0;
         return (4.0 / 3.0) * PI * Math.pow(radius, 3);
     }
 
-    // menghitung luas permukaan kubus
-    public double luasPermukaanKubus() {
-        return 6 * Math.pow(sisi, 2);
-    }
-
-    // menghitung luas permukaan balok
-    public double luasPermukaanBalok() {
-        return 2 * (panjang * lebar + panjang * tinggi + lebar * tinggi);
-    }
-
-    // menghitung luas permukaan bola
+    // Menghitung luas permukaan Bola
     public double luasPermukaanBola() {
         double radius = diameter / 2.0;
         return 4 * PI * Math.pow(radius, 2);
     }
-}*/
+
+    // Menghitung keliling Bola
+    public double kelilingBola() {
+        return PI * diameter;
+    }
+}
